f0bb8739e34203459a86a45da3fdec21ea3c5617
nannnnua <nannnnua@gmail.com>
2024-06-23 11:05:11 +0900

    Link Youtube

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-// https://github.com/aamirafridi/jQuery.Marquee
 $(document).ready(function() {
   $('.marquee').marquee({
     duration: 20000,
@@ -10,3 +9,24 @@ $(document).ready(function() {
     startVisible: true
   });
 });
+
+// Mouse Event
+// document.addEventListener('mousemove', (e) => {
+//   const circles = document.querySelectorAll('.mouseMove');
+//   circles.forEach(circle => {
+//     const rect = circle.getBoundingClientRect();
+//     const circleX = rect.left + rect.width / 2;
+//     const circleY = rect.top + rect.height / 2;
+//     const distX = e.clientX - circleX;
+//     const distY = e.clientY - circleY;
+//     const dist = Math.sqrt(distX * distX + distY * distY);
+
+//     if (dist < 200) {
+//       const moveX = (distX / dist) * 20;
+//       const moveY = (distY / dist) * 20;
+//       circle.style.transform = `translate(${moveX}px, ${moveY}px)`;
+//     } else {
+//       circle.style.transform = 'translate(0, 0)';
+//     }
+//   });
+// });
